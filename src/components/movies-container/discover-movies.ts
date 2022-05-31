e9d43f1d399b0270bc08380308be7e733defd3a5
Csenge <jakabos11@gmail.com>
2022-05-31 22:28:57 +0200

    feat(dual-search-engine): query

---
+++ src/components/movies-container/discover-movies.ts
@@ -0,0 +1,22 @@
+import { gql } from "@apollo/client";
+
+export const DISCOVER_MOVIES = gql`
+  query DiscoverMovies($genres: [ID!]!) {
+    discoverMovies(filter: {  withGenres: { include: $genres } }) {
+    id
+    name
+    overview
+    releaseDate
+    score
+    socialMedia{
+      imdb
+    }
+    genres {
+      name
+      id
+    }
+    poster {
+      huge
+    }
+  }
+}`;
